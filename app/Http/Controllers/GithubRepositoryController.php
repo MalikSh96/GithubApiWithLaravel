8dd227f80b623057d1054387193a14ce3904ab0a
Malik Sharfo <malik96sharfo@hotmail.com>
2021-10-03 18:06:26 +0200

    removed unused code

--- app/Http/Controllers/GithubRepositoryController.php
+++ app/Http/Controllers/GithubRepositoryController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
-use App\Models\Githubrepository;
 use App\Helpers\IRequestData;
 use App\Helpers\IDataBaseHandler;
 
